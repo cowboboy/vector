0281b0ed132c23586e80faabcaa676f1a218a5c4
cowboboy <r2d2tv@yandex.ru>
2022-01-29 01:30:48 +0500

    Small fixes

--- main.py
+++ main.py
@@ -79,4 +79,14 @@ class Vector:
     def __imul__(self, other):
         return self * other
 
+    def __eq__(self, other):
+        """
+        Output True if double coords of Vectors are equal, False in another cases
+        """
+        return all([n[0] == n[1] for n in zip(self.__coords, other.__coords)])
 
+    def __hash__(self):
+        return hash(tuple(self.__coords))
+
+    def __getitem__(self, item):
+        return self.__coords[item]
